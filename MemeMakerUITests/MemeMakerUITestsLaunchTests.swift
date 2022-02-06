cc314bfb4697a2be44cf9a27fa67485254dc4ef2
dahae0320 <ldh990320ldh@gmail.com>
2022-02-06 19:51:01 +0900

    Initial Commit

---
+++ MemeMakerUITests/MemeMakerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MemeMakerUITestsLaunchTests.swift
+//  MemeMakerUITests
+//
+//  Created by Da Hae Lee on 2022/02/06.
+//
+
+import XCTest
+
+class MemeMakerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
